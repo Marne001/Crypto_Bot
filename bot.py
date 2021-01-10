d4ddefafc54dd598c92683a0ab1fb265ec972623
Marné <vanzyl.marne007@gmail.com>
2021-01-10 18:46:23 +0200

    Added new parameters and Telegram bot for notifications

--- bot.py
+++ bot.py
@@ -2,14 +2,32 @@ import websocket, json, pprint, talib, numpy
 from binance.client import Client
 from binance.enums import *
 import config
+import requests
+
+#Telagram Bot
+def telegram_bot_sendtext(bot_message):
+    bot_token = config.BOT_API
+    bot_chatID = config.CHAT_ID #Group
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+#Get Bought value:
+with open('buy_value.txt') as f:
+    for value in f:
+        pass
+    last_buy_value = float(value) #last value = value crypto was bought at
+f.close()
+print("ZIL BOUGHT VALUE: {}".format(last_buy_value))
+
 
 SOCKET = "wss://stream.binance.com:9443/ws/zilusdt@kline_1m"
 
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
-RSI_OVERSOLD = 30
+RSI_OVERSOLD = 45
 TRADE_SYMBOL = 'ZILUSDT'
-TRADE_QUANTITY = 10
+TRADE_QUANTITY = 192
 
 closes = []
 
@@ -23,6 +41,7 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
         order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
         print(order)
     except Exception as e:
+        print("an exception occurd - {}".format(e))
         return False
     return True
 
@@ -32,8 +51,9 @@ def on_open(ws):
 def on_close(ws):
     print('closed connection')
 
+
 def on_message(ws, message):
-    global closes
+    global closes, in_position
     print('recieved message')
     json_message = json.loads(message)
     pprint.pprint(json_message)
@@ -43,11 +63,14 @@ def on_message(ws, message):
     is_candle_closed = candle['x']
     close = candle['c']
 
+    
+
     if is_candle_closed:
         print('candle closed at {}'. format(close))
         closes.append(float(close))
         print("closes")
         print(closes)
+
 
         if len(closes) > RSI_PERIOD:
             np_closes = numpy.array(closes)
@@ -57,17 +80,26 @@ def on_message(ws, message):
             last_rsi = rsi[-1]
             print("the currrent rsi is {}".format(last_rsi))
 
-            if last_rsi > RSI_OVERBOUGHT:
+
+            if (last_rsi > RSI_OVERBOUGHT) and (float(close) > last_buy_value) :
                 if in_position:
                     print("OVERBOUGHT! Sell! Sell! Sell!")
                     #BINANCE SELL LOGIC
                     order_succeeded =  order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
                         in_position = False
+
+                        ##Write sell value to text file
+                        with open("sell_value.txt", "a") as f:
+                            f.write(str(close) + "\n")
+                            f.close()
+
+                        #Bot Message to telegram
+                        telegram_bot_sendtext("Bought Zil At: " + str(close))
                 else:
                     print("Is overbought, but we don't own any. Cant sell!")
             
-            if last_rsi < RSI_OVERSOLD:
+            if (last_rsi < RSI_OVERSOLD) and (float(close) <= 0.075):
                 if in_position:
                     print("It is oversold, but already own. Can't Buy!")
                 else:
@@ -76,6 +108,14 @@ def on_message(ws, message):
                     order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
                         in_position = True
+
+                        #Write value to text file
+                        with open("buy_value.txt", "a") as f:
+                            f.write(str(close) + "\n")
+                            f.close()
+                        
+                        #Bot Message to telegram
+                        telegram_bot_sendtext("Sold Zil At: " + str(close))
                     
 
 
---
+++ read.py
@@ -0,0 +1,7 @@
+with open('value.txt') as f:
+    for value in f:
+        pass
+    last_value = value
+
+print(last_value)
+
